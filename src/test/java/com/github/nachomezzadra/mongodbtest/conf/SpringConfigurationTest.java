1dcec41f337d586e51a16583916305d208d4989b
Nacho Mezzadra <nacho@penque.fibertel.com.ar>
2014-06-16 23:29:06 -0300

    Initial commit

---
+++ src/test/java/com/github/nachomezzadra/mongodbtest/conf/SpringConfigurationTest.java
@@ -0,0 +1,21 @@
+package com.github.nachomezzadra.mongodbtest.conf;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+
+import com.github.nachomezzadra.mongodbtest.BaseSpringTest;
+
+public class SpringConfigurationTest extends BaseSpringTest  {
+
+	@Autowired
+	ApplicationContext context;
+
+	
+	@Test
+	public void shouldProperlyLoadSpringContext() throws Exception {
+		assertNotNull(this.context);
+	}
+}
